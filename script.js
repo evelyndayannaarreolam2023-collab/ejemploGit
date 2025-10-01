fbfb9dcc3e7e3fc56689e99f4f70280aa4474306
evelyndayannaarreolam2023-collab <evelyn.dayanna.arreola.m.2023@cetis162.edu.mx>
2025-09-30 22:42:43 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleccionamos el botón
+const boton = document.getElementById("btn-saludo");
+
+// Al hacer clic muestra un mensaje
+boton.addEventListener("click", () => {
+  alert("¡Hola! Bienvenido a mi perfil 😃");
+});
